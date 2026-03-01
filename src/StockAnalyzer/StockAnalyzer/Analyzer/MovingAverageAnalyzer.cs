6c44d5644640fd7080840c97a9f9dfe4b170a667
Yamada Saito <yamada.saito00@gmail.com>
2026-03-01 10:03:43 +0900

    移動平均の計算処理を実装

---
+++ src/StockAnalyzer/StockAnalyzer/Analyzer/MovingAverageAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿namespace StockAnalyzer.Analyzer;
+
+public static class MovingAverageAnalyzer
+{
+    public static List<double?> CalculateSma(IReadOnlyList<double> values, int period)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(period);
+
+        var result = new List<double?>(values.Count);
+        double sum = 0;
+
+        for (int i = 0; i < values.Count; i++)
+        {
+            sum += values[i];
+
+            if (i >= period)
+            {
+                sum -= values[i - period];
+            }
+
+            result.Add(i >= period - 1 ? sum / period : null);
+        }
+
+        return result;
+    }
+}
