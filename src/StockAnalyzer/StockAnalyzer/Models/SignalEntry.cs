48c7d37a3041a38be03e764e8d893b2f72bc09dd
Yamada Saito <yamada.saito00@gmail.com>
2026-03-03 06:22:17 +0900

    SignalEntryクラスを追加

---
+++ src/StockAnalyzer/StockAnalyzer/Models/SignalEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace StockAnalyzer.Models;
+
+public sealed class SignalEntry
+{
+    public string Date { get; set; } = string.Empty;
+    public SignalType Type { get; set; }
+    public double Price { get; set; }
+    public double Ma { get; set; }
+}
