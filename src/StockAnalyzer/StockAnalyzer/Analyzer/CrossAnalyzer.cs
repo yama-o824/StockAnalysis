fdea469d61b2143c95eaaf009dce292b28bf69c3
Yamada Saito <yamada.saito00@gmail.com>
2026-03-12 06:38:59 +0900

    リファクタ：検出部をメソッド化

--- src/StockAnalyzer/StockAnalyzer/Analyzer/CrossAnalyzer.cs
+++ src/StockAnalyzer/StockAnalyzer/Analyzer/CrossAnalyzer.cs
@@ -17,46 +17,51 @@ public static class CrossAnalyzer
 
             if (prev.MA75 is null || current.MA75 is null) continue;
 
-            var prevClose = prev.Close;
-            var prevMa = prev.MA75.Value;
-            var currentClose = current.Close;
-            var currentMa = current.MA75.Value;
-
-            var prevDiff = prevClose - prevMa;
-            var currentDiff = currentClose - currentMa;
-
-            // BUY: ゴールデンクロス
-            if (prevDiff < 0 && currentDiff > 0)
-            {
-                signals.Add(new SignalEntry
-                {
-                    Date = current.Date,
-                    Type = SignalType.Buy,
-                    PrevPrice = prevClose,
-                    PrevMa = prevMa,
-                    PrevDiff = prevDiff,
-                    Price = currentClose,
-                    Ma = currentMa,
-                    CurrentDiff = currentDiff
-                });
-            }
-            // SELL: デッドクロス
-            else if (prevDiff > 0 && currentDiff < 0)
-            {
-                signals.Add(new SignalEntry
-                {
-                    Date = current.Date,
-                    Type = SignalType.Sell,
-                    PrevPrice = prevClose,
-                    PrevMa = prevMa,
-                    PrevDiff = prevDiff,
-                    Price = currentClose,
-                    Ma = currentMa,
-                    CurrentDiff = currentDiff
-                });
-            }
+            DetectCrossSignalCore(signals, prev, current);
         }
 
         return signals;
+    }
+
+    private static void DetectCrossSignalCore(List<SignalEntry> signals, PriceRow prev, PriceRow current)
+    {
+        var prevClose = prev.Close;
+        var prevMa = prev.MA75.Value;
+        var currentClose = current.Close;
+        var currentMa = current.MA75.Value;
+
+        var prevDiff = prevClose - prevMa;
+        var currentDiff = currentClose - currentMa;
+
+        // BUY: ゴールデンクロス
+        if (prevDiff < 0 && currentDiff > 0)
+        {
+            signals.Add(new SignalEntry
+            {
+                Date = current.Date,
+                Type = SignalType.Buy,
+                PrevPrice = prevClose,
+                PrevMa = prevMa,
+                PrevDiff = prevDiff,
+                Price = currentClose,
+                Ma = currentMa,
+                CurrentDiff = currentDiff
+            });
+        }
+        // SELL: デッドクロス
+        else if (prevDiff > 0 && currentDiff < 0)
+        {
+            signals.Add(new SignalEntry
+            {
+                Date = current.Date,
+                Type = SignalType.Sell,
+                PrevPrice = prevClose,
+                PrevMa = prevMa,
+                PrevDiff = prevDiff,
+                Price = currentClose,
+                Ma = currentMa,
+                CurrentDiff = currentDiff
+            });
+        }
     }
 }
